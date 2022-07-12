2c82742d05c967caf8ae2ad5c90ff1c20d2818a8
Tomaz Geddes de Almeida <tomaz@Tomazs-MacBook-Air.local>
2022-07-12 14:35:42 +0100

    mostly web server that echoes messages

--- racket-server/main.rkt
+++ racket-server/main.rkt
@@ -1,13 +1,37 @@
 #lang racket
+(require net/rfc6455)
+
+;; (define port 8081)
+;; (ws-serve #:port port (lambda (c s) (ws-send! c "Hello world!")))
 
 (module+ main
-  (require net/rfc6455)
-
   (define port 8081)
+  (define idle-timeout #f)
+  (command-line #:once-each
+                ["--timeout" SECONDS "Set per-connection idle timeout"
+                 (set! idle-timeout (string->number SECONDS))]
+                ["--port" PORT "Set service port"
+                 (set! port (string->number PORT))])
 
-  (ws-serve #:port port (lambda (c s) (ws-send! c "Hello world!")))
+  (define (connection-handler c state)
+    (define id (gensym 'conn))
+    (printf "~a: Connection received\n" id)
+    (let loop ()
+      (match (ws-recv c #:payload-type 'text)
+        [(? eof-object?) (void)]
+        [m
+         (printf "~a: Received: ~v\n" id m)
+         (ws-send! c m)
+         (loop)]))
+    (printf "~a: Close status: ~v ~v\n"
+            id
+            (ws-conn-close-status c)
+            (ws-conn-close-reason c))
+    (ws-close! c)
+    (printf "~a: Connection closed\n" id))
 
-  )
-
-
-
+  (when idle-timeout (ws-idle-timeout idle-timeout))
+  (define stop-service (ws-serve #:port port connection-handler))
+  (printf "Server running. Hit enter to stop service.\n")
+  (void (read-line))
+  (stop-service))
